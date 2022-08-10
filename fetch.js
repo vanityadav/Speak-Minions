1105d2b2e713adfd06806bb8dd1777c9431984b0
vanityadav <110538930+vanityadav@users.noreply.github.com>
2022-08-10 13:59:56 +0530

    Initial Commit

---
+++ fetch.js
@@ -0,0 +1,13 @@
+let buttonClicked = document.getElementsByClassName('button')[0]
+buttonClicked.addEventListener("click", translate)
+
+function translate() {
+let newText = document.getElementsByClassName('translated-text')[0]
+    
+fetch(`https://api.funtranslations.com/translate/minion.json?text=${document.getElementsByClassName('input')[0].value}`)
+.then(value => value.json())
+.then(newvalue => newText.innerText = newvalue.contents.translated)
+console.log(newText.value)
+}
+
+
