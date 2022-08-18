d7b3db9fff25ea16421244d57ce51baad8508bf8
vanityadav <110538930+vanityadav@users.noreply.github.com>
2022-08-18 16:48:02 +0530

    Updated Page

--- fetch.js
+++ fetch.js
@@ -4,7 +4,8 @@ buttonClicked.addEventListener("click", translate)
 function translate() {
 let newText = document.getElementsByClassName('translated-text')[0]
     
-fetch(`https://api.funtranslations.com/translate/minion.json?text=${document.getElementsByClassName('input')[0].value}`)
+fetch(`https://api.funtranslations.com/translate/minion.json?text=
+    ${document.getElementsByClassName('input')[0].value}`)
 .then(value => value.json())
 .then(newvalue => newText.innerText = newvalue.contents.translated)
 console.log(newText.value)
